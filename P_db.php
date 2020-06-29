b74d8fd4d5cacdf12e3e683497bc53319a04d85e
marco-martinho <37421542+marco-martinho@users.noreply.github.com>
2020-06-29 20:16:52 +0200

    Update P_db.php

--- P_db.php
+++ P_db.php
@@ -5,21 +5,4 @@ mysqli_set_charset($con,"utf8");
 
 ?>
 
-<?php
-/*
-$host_name = 'db719843115.db.1and1.com';
-$database = 'db719843115';
-$user_name = 'dbo719843115';
-$password = 'epilifocram69@Inkluzive';
 
-$connect = mysql_connect($host_name, $user_name, $password, $database);
-
-
-if (mysql_errno()) {
-    die('<p>Verbindung zum MySQL Server fehlgeschlagen: '.mysql_error().'</p>');
-} else {
-    echo '<p>Verbindung zum MySQL Server erfolgreich aufgebaut.</p >';
-}
-
-?>
-*/
